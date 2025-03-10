4d510150e2df6360d223963ffd52cd0037aca17b
erfanmazid <erfanmazidm@gmail.com>
2025-03-10 21:52:34 +0330

    fix: fix file name

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  basePath: "/Portfolio",
+  assetPrefix: "/Portfolio/",
+  images: {
+    unoptimized: true,
+  },
+};
+
+export default nextConfig;
