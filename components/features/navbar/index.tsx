e9ca8cdf010203405c0b464fae0e5655cc41a277
erfanmazid <erfanmazidm@gmail.com>
2025-03-11 21:19:42 +0330

    fix fix url link

--- components/features/navbar/index.tsx
+++ components/features/navbar/index.tsx
@@ -5,16 +5,17 @@ import { usePathname } from "next/navigation";
 
 function Navbar() {
   const pathname = usePathname();
+  console.log(pathname);
 
   return (
     <div className="fixed bottom-0 left-0 right-0 bg-[#2a2a2cBF]/75 h-16 border-2 border-light-gray rounded-t-3xl z-50 backdrop-blur-md shadow-lg lg:hidden">
       <ul className="flex gap-3 justify-center items-center h-full text-sm text-gray-300">
         {[
           { href: "/", label: "درباره من" },
-          { href: "/resume", label: "رزومه" },
-          { href: "/portfolio", label: "نمونه کار" },
-          { href: "/blog", label: "بلاگ" },
-          { href: "/contact", label: "ارتباط با من" },
+          { href: "/resume/", label: "رزومه" },
+          { href: "/portfolio/", label: "نمونه کار" },
+          { href: "/blog/", label: "بلاگ" },
+          { href: "/contact/", label: "ارتباط با من" },
         ].map(({ href, label }) => (
           <li key={href}>
             <Link
